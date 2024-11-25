d35ff8def60e5d92db46d406c8011ea7367ebd54
xraystyle1980 <matt.trice@gmail.com>
2024-11-25 14:31:10 -0500

    looking for favicon

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
         <div className="container mx-auto py-4 px-4">
 
           <div className='flex justify-between'>
-            <div className="text-sm text-Neutral-0 flex flex-row items-center justify-center">
+            <div className="text-xs text-gray-400 flex flex-row items-center justify-center">
               Powered by <NyanCat className="w-9 mx-1" />
               {/* Nyan Cat by Sem Schilder from <a href="https://thenounproject.com/browse/icons/term/nyan-cat/" target="_blank" title="Nyan Cat Icons">Noun Project</a> (CC BY 3.0) */}
             </div>
